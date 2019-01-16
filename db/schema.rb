ff6b63ea40a527b8d481674e86ec7a51d1aa2aad
Pekoiq <evangarden@gmail.com>
2019-01-16 15:26:28 -0500

    make basic Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_11_191614) do
+ActiveRecord::Schema.define(version: 2019_01_16_202436) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
